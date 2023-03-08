c56a9156c69b55787ccde782c1cbd9d9b74ce46d
Leahnicoles <lnsimonn@hotmail.com>
2023-03-08 13:32:22 +0000

    fix: initial setup

--- app.js
+++ app.js
@@ -3,9 +3,10 @@
 const getCountryByName = (countryName) => {
     fetch(`https://restcountries.com/v3.1/name/${countryName}`)
     .then(response => response.json())
-    .then(data => console.log(data));
-}
+    .then(data => console.log(data)) 
+    }
 
-getCountryByName("Canada");
+
+getCountryByName("Peru");
 
 
