50856199377ee33e2c5d844a1468b9b0723eb225
Leahnicoles <lnsimonn@hotmail.com>
2023-03-08 19:36:22 +0000

    mvp completed

--- app.js
+++ app.js
@@ -3,10 +3,44 @@
 const getCountryByName = (countryName) => {
     fetch(`https://restcountries.com/v3.1/name/${countryName}`)
     .then(response => response.json())
-    .then(data => console.log(data)) 
+    .then(data => {
+        let country = data[0];
+        let section = document.getElementById("countries");
+        let name = document.createElement("h3"); //Heading
+        name.innerHTML = `Country Name: ${country.name.common}`; //Get Countries Name
+        section.appendChild(name); //Connect Append Child
+        let population = document.createElement("h3")
+        population.innerHTML = `Population: ${country.population}`
+        section.appendChild(population);
+    } ) 
+    
+}
+
+//getCountryByName("Peru"); 
+
+   
+
+    const getAllCountries = () => {
+        fetch(`https://restcountries.com/v3.1/all`)
+        .then(response => response.json())
+        .then(data => {
+            let section = document.getElementById("all_countries");
+            data.forEach(country => {
+            let name = document.createElement("h3");
+            name.innerHTML = `Country Name: ${country.name.common}`;
+            section.appendChild(name);
+            let population = document.createElement("h3")
+            population.innerHTML = `Population: ${country.population}`;
+            section.appendChild(population);
+        });
+              
+        });
+        
     }
+    document.addEventListener("DOMContentLoaded", getAllCountries);
 
 
-getCountryByName("Peru");
+
+
 
 
